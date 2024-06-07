691e628c4f1ece0bdd99d8238a83e0776296561d
Pushpesh Kumar <107409499+Pushpesh987@users.noreply.github.com>
2024-06-07 12:41:04 +0530

    Update statistics.py

--- statistics.py
+++ statistics.py
@@ -1,4 +1,17 @@
- def mean(xs: List[float]) -> float:
+from typing import Tuple
+from typing import List
+import math
+
+Vector = List[float]
+
+def standard_deviation(xs: List[float]) -> float:
+    """The standard deviation is the square root of the variance"""
+    return math.sqrt(variance(xs))
+
+def addTwoNum(a,b):
+    return a+b
+
+def mean(xs: List[float]) -> float:
     return sum(xs) / len(xs)
 
 def covariance(xs: List[float], ys: List[float]) -> float:
@@ -14,17 +27,23 @@ def correlation(xs: List[float], ys: List[float]) -> float:
     if stdev_x > 0 and stdev_y > 0:
         return covariance(xs, ys) / stdev_x / stdev_y
     else:
-        return 0  
-
-def standard_deviation(xs: List[float]) -> float:
-    """The standard deviation is the square root of the variance"""
-    return math.sqrt(variance(xs))
+        return 0 
 
 
 def de_mean(xs: List[float]) -> List[float]:
     """Translate xs by subtracting its mean (so the result has mean 0)"""
     x_bar = mean(xs)
     return [x - x_bar for x in xs]
+
+def dot(v: Vector, w: Vector) -> float:
+    """Computes v_1 * w_1 + ... + v_n * w_n"""
+    assert len(v) == len(w), "vectors must be same length"
+
+    return sum(v_i * w_i for v_i, w_i in zip(v, w))
+
+def sum_of_squares(v: Vector) -> float:
+    """Returns v_1 * v_1 + ... + v_n * v_n"""
+    return dot(v, v)
 
 def variance(xs: List[float]) -> float:
     """Almost the average squared deviation from the mean"""
